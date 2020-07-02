40632e9c673b31c1c7ae139ec9c669b95c01228d
Avijit <avi_roy1988@hotmail.com>
2020-07-02 13:19:36 +0200

    deploy code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import {SearchBox} from "./components/search-box/search-box.component"
 import "./App.css";
 
 class App extends Component {
@@ -16,6 +17,10 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = e =>{
+    this.setState({searchField: e.target.value});
+  }
+
   render() {
     const {monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -23,10 +28,10 @@ class App extends Component {
       );
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="Search monster"
-          onChange={e => this.setState({ searchField: e.target.value })}
+        <h1>Mosters Rolodex</h1>
+        <SearchBox
+          placeholder='Search Monster'
+          handleChange={this.handleChange} 
         />
         <CardList monsters={filteredMonsters} />
       </div>
