68bd4654d35cac435c36acbadd5d74265300d18a
Avijit <avi_roy1988@hotmail.com>
2020-07-02 13:28:07 +0200

    Updates

---
+++ precache-manifest.2d3164c7a033f6d407d40184267cb51c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04edea8319420b2f71bea3ce6a82e32",
+    "url": "/react-monsters-app/index.html"
+  },
+  {
+    "revision": "dd1fbd785fb35ab2ca09",
+    "url": "/react-monsters-app/static/css/main.586ca334.chunk.css"
+  },
+  {
+    "revision": "76e1e33d0dc86b7a1eb3",
+    "url": "/react-monsters-app/static/js/2.2e94f083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-app/static/js/2.2e94f083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1fbd785fb35ab2ca09",
+    "url": "/react-monsters-app/static/js/main.4cc9eb39.chunk.js"
+  },
+  {
+    "revision": "a8dc46d72a3b53b46049",
+    "url": "/react-monsters-app/static/js/runtime-main.01a22548.js"
+  }
+]);
